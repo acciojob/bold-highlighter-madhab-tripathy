624d6d81998fc43f1bf377139914b96bb3c4c6bd
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-20 00:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
+    document.querySelectorAll("strong").forEach((element) => {
+        element.classList.add("highlight")
+    })
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+    document.querySelectorAll("strong").forEach((element) => {
+        element.classList.remove("highlight")
+    })    
 }
